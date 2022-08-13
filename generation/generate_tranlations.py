9752ce492bec0da3226efbb70d6c705b0f1bc764
Darmo117 <darmo117@gmail.com>
2022-08-13 13:15:33 +0200

    translations now sorted in en_us.json

--- generation/generate_tranlations.py
+++ generation/generate_tranlations.py
@@ -11,11 +11,10 @@ translations = {
     'itemGroup.jab.item_group': 'Jusqu’au Bout',
 }
 
-for f in (utils.ASSETS_DIR / 'blockstates').glob('*.json'):
+for f in sorted((utils.ASSETS_DIR / 'blockstates').glob('*.json')):
     block_id: str = os.path.splitext(f.name)[0]
     translations['block.jab.' + block_id] = block_id.replace('_', ' ').title()
 
-# TODO sort keys
 with (utils.ASSETS_DIR / 'lang' / 'en_us.json').open(mode='w', encoding='utf8') as f:
     json.dump(translations, f, ensure_ascii=False, indent=2)
 print(f'Done in {(time.time() - start) * 1000} ms')
